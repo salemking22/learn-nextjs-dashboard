8a4eb6427f9056dd1eea326f8c5c57accdea4080
salemking22 <brynjerel@gmail.com>
2025-11-18 12:34:29 +0200

    Add signIn to AuthButton

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,9 +1,13 @@
-'use client'
+"use client";
+import { signIn } from "next-auth/react";
 
 export default function AuthButton() {
   return (
-    <button className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-500">
+    <button
+      onClick={() => signIn("github")}
+      className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-500"
+    >
       Sign In
     </button>
-  )
+  );
 }
